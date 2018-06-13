a7925b57f9a5adba049f2978d8a999dd0d78d203
Vinicius Oliveira <vinicius.developer@outlook.com>
2018-06-13 17:45:27 -0300

    Subindo todos os arquivos feitos até agora do react

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import MenuSuperior from './components/MenuSuperior/MenuSuperior'
+
+class App extends Component {
+  render() {
+    return (
+        <div>
+          <MenuSuperior />
+        </div>
+    );
+  }
+}
+
+export default App;
